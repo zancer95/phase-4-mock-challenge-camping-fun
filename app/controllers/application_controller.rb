3f7b0dd2c7dfe245a1f8cc3a3462149827d625c9
ZhiLeong <saber_zhileong@hotmail.com>
2023-02-07 15:27:34 -0500

    commit tree a3ab39a0f5d23c4fb813973b01b9e5fde3dd58f7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
+  private
+
+  def render_not_found(invalid)
+    render json: { error: "#{invalid.model} not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
